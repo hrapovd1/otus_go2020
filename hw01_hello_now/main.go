d787b6cebd543da6e3935728be20bebd79146883
Dmitry Khrapov <hrapovd@gmail.com>
2020-08-28 23:21:54 +0300

    I've learned how to get net time and print it

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+)
 
 func main() {
+	// Запрашиваю время на сервере NTP
+	netTime, _ := ntp.Time("0.ru.pool.ntp.org")
+	// Получаю локальное время
 	var now = time.Now()
 	fmt.Printf("Current time: %02d:%02d:%02d\n", now.Hour(), now.Minute(), now.Second())
+	fmt.Printf("Exact time: %02d:%02d:%02d\n", netTime.Hour(), netTime.Minute(), netTime.Second())
 }
