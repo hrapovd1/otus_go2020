36073de140fd64709cc244b0ba04f10c9316bea8
Dmitry Khrapov <hrapovd@gmail.com>
2020-08-29 12:25:27 +0300

    test PASS, display time in Local, but wrong time zone ID

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -14,26 +14,26 @@ func main() {
 	now := time.Now()                // Получаю локальное время
 
 	// Вариант вывода времени UTC, тест не проходит
-	location, _ := time.LoadLocation("UTC") // Устанавливаю временную зону на UTC
-
-	localTime := time.Date(now.Year(), now.Month(), now.Day(),
-		now.Hour(), now.Minute(), now.Second(), 0, time.Local)
-
-	netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-		netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.Local)
-
-	fmt.Printf("current time: %v\n", localTime.In(location))
-	fmt.Printf("exact time: %v\n", netTime.In(location))
-
-	// Вариант вывода локального времени с показом зоны UTC, тест проходит !
 	/*
+		location, _ := time.LoadLocation("UTC") // Устанавливаю временную зону на UTC
+
 		localTime := time.Date(now.Year(), now.Month(), now.Day(),
-			now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
+			now.Hour(), now.Minute(), now.Second(), 0, time.Local)
 
 		netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-			netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.UTC)
+			netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.Local)
 
 		fmt.Printf("current time: %v\n", localTime.In(location))
 		fmt.Printf("exact time: %v\n", netTime.In(location))
 	*/
+
+	// Вариант вывода локального времени с показом зоны UTC, тест проходит !
+	localTime := time.Date(now.Year(), now.Month(), now.Day(),
+		now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
+
+	netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
+		netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.UTC)
+
+	fmt.Printf("current time: %v\n", localTime)
+	fmt.Printf("exact time: %v\n", netTime)
 }
