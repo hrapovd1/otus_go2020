2cb4a092fb6ee3925b0f9b97560b087d405328ce
Dmitry Khrapov <hrapovd@gmail.com>
2020-08-29 13:47:42 +0300

    Add error handler for NTP. Fix go.mod. Final edition.

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/beevik/ntp"
@@ -10,18 +11,24 @@ import (
 func main() {
 	const ntpServer = "0.ru.pool.ntp.org"
 
-	netNow, _ := ntp.Time(ntpServer) // Запрашиваю время на сервере NTP
-	now := time.Now()                // Получаю локальное время
+	netNow, err := ntp.Time(ntpServer) // Запрашиваю время на сервере NTP, важно выполнить раньше локального запроса.
+	if err != nil {
+		log.Fatalf("NTP error: %v\n", err)
+	}
+	now := time.Now() // Получаю локальное время
 
 	// Вариант вывода времени UTC, тест не проходит
 	/*
-		location, _ := time.LoadLocation("UTC") // Устанавливаю временную зону на UTC
+		location, err := time.LoadLocation("UTC") // Устанавливаю временную зону на UTC
+		if err != nil {
+			log.Fatalf("LoadLocation error: %v\n", err)
+		}
 
 		localTime := time.Date(now.Year(), now.Month(), now.Day(),
-			now.Hour(), now.Minute(), now.Second(), 0, time.Local)
+			now.Hour(), now.Minute(), now.Second(), 0, time.Local) //Формирую строку локального времени
 
 		netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-			netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.Local)
+			netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.Local) //Формирую строку сетевого времени
 
 		fmt.Printf("current time: %v\n", localTime.In(location))
 		fmt.Printf("exact time: %v\n", netTime.In(location))
@@ -29,10 +36,10 @@ func main() {
 
 	// Вариант вывода локального времени с показом зоны UTC, тест проходит !
 	localTime := time.Date(now.Year(), now.Month(), now.Day(),
-		now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
+		now.Hour(), now.Minute(), now.Second(), 0, time.UTC) //Формирую строку локального времени
 
 	netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-		netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.UTC)
+		netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.UTC) //Формирую строку сетевого времени
 
 	fmt.Printf("current time: %v\n", localTime)
 	fmt.Printf("exact time: %v\n", netTime)
