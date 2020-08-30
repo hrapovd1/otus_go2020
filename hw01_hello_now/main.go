e4250e38888c0045d6c4045c9db5345cad288c81
Dmitry Khrapov <hrapovd@gmail.com>
2020-08-30 10:46:47 +0300

    Fix PR comments.

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -15,32 +15,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("NTP error: %v\n", err)
 	}
-	now := time.Now() // Получаю локальное время
+	localNow := time.Now() // Получаю локальное время
 
-	// Вариант вывода времени UTC, тест не проходит
-	/*
-		location, err := time.LoadLocation("UTC") // Устанавливаю временную зону на UTC
-		if err != nil {
-			log.Fatalf("LoadLocation error: %v\n", err)
-		}
-
-		localTime := time.Date(now.Year(), now.Month(), now.Day(),
-			now.Hour(), now.Minute(), now.Second(), 0, time.Local) //Формирую строку локального времени
-
-		netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-			netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.Local) //Формирую строку сетевого времени
-
-		fmt.Printf("current time: %v\n", localTime.In(location))
-		fmt.Printf("exact time: %v\n", netTime.In(location))
-	*/
-
-	// Вариант вывода локального времени с показом зоны UTC, тест проходит !
-	localTime := time.Date(now.Year(), now.Month(), now.Day(),
-		now.Hour(), now.Minute(), now.Second(), 0, time.UTC) //Формирую строку локального времени
-
-	netTime := time.Date(netNow.Year(), netNow.Month(), netNow.Day(),
-		netNow.Hour(), netNow.Minute(), netNow.Second(), 0, time.UTC) //Формирую строку сетевого времени
-
-	fmt.Printf("current time: %v\n", localTime)
-	fmt.Printf("exact time: %v\n", netTime)
+	fmt.Printf("current time: %v\n", localNow.UTC()) // Вывожу локальное время ПК в UTC
+	fmt.Printf("exact time: %v\n", netNow.UTC())     // Вывожу сетевое время в UTC
 }
