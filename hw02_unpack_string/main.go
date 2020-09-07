b7fed09597fc51e0e4dfd4b4a227501b5ea6b22e
Dmitry Khrapov <hrapovd@gmail.com>
2020-09-07 21:51:01 +0300

    worked intermedia result.

--- hw02_unpack_string/main.go
+++ hw02_unpack_string/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"unicode"
@@ -15,60 +16,94 @@ func Unpack(str string) (string, error) {
 	}
 
 	var strOut strings.Builder //Output string
+	isDouble := false          //For escaped symbols like \n
 
 	// Loop work with each symbol
 	for pos := 0; pos < len(str); pos++ {
+
 		switch {
+		// Check first symbol to digital and \
 		case pos == 0:
 			switch {
 			case unicode.IsDigit(rune(str[pos])): // check isn't first digit
 				return "", errors.New("wrong string, it can't start from digit")
-			case unicode.IsDigit(rune(str[pos+1])):
+			case unicode.IsDigit(rune(str[pos+1])): // check if next is digit than need to unpack symbol
 				break
-			case unicode.IsSymbol('\\'):
+			case string(str[pos]) == `\`: // check if it's \ than need to check next symbol
 				break
-			default:
+			default: // if it isn't digit, \ or next digit than simple write to out string
 				strOut.WriteString(string(str[pos]))
 			}
 
+		// Work with another symbols exclude first
 		case pos != 0:
+			notLastChar := true    // Flag is not last char
+			if pos == len(str)-1 { // If it's last char that set flag
+				notLastChar = false
+			}
+			prevChar, currChar := str[pos-1], str[pos]
+
 			switch {
-			case unicode.IsDigit(rune(str[pos])):
+			// if it's digit
+			case unicode.IsDigit(rune(currChar)):
 				// Check isn't numeric
-				// TODO: check str[pos] to last symbol in string
-				if unicode.IsDigit(rune(str[pos-1])) || unicode.IsDigit(rune(str[pos+1])) {
+				if notLastChar && unicode.IsDigit(rune(str[pos+1])) || unicode.IsDigit(rune(str[pos-1])) {
 					return "", errors.New("wrong string, it can be only digit, not numeric")
 				}
-
-				prevChar, currChar := str[pos-1], str[pos]
+				// convert string digit to int
 				currDigit, err := strconv.Atoi(string(currChar))
 				if err != nil {
 					return "", err
 				}
 
 				switch {
+				// if digit 0 than skip previous symbol
 				case currDigit == 0:
 					break
+				// if digit more than 0 - unpack it
 				case currDigit > 0:
+					writedChar := string(prevChar)
+					// if previos was escape sequence
+					if isDouble {
+						writedChar = str[pos-2 : pos]
+					}
 					for count := currDigit; count > 0; count-- {
-						strOut.WriteString(string(prevChar))
+						// unpack and write to output
+						strOut.WriteString(writedChar)
 					}
 				}
-			default:
-				strOut.WriteString(string(str[pos]))
-			}
-			/*
-				switch {
-				case "0" == strconv.FormatUint(uint64(str[pos]), 10): // if digit is zero than skip symbol
-					continue
+			// if it's \ than processed it
+			case string(currChar) == `\`:
+				// if previos was \
+				if string(prevChar) == `\` {
+					strOut.WriteString(string(prevChar))
 				}
-			*/
+				break
+			// if it isn't digit, \ or escape
+			default:
+				if string(prevChar) == `\` {
+					// if it's escape sequence set flag
+					if notLastChar && unicode.IsDigit(rune(str[pos+1])) {
+						isDouble = true
+						break
+					}
+					strOut.WriteString(string(prevChar))
+				}
+				if notLastChar && unicode.IsDigit(rune(str[pos+1])) {
+					break
+				}
+				strOut.WriteString(string(currChar))
+			}
 		}
 	}
 	return strOut.String(), nil
 }
 
 func main() {
-	const inStr string = "test2Str" // Input string
-	fmt.Printf("Unpacked string: %s", Unpack(inStr))
+	const inStr string = `\_\3\:wtest2S\\t` // Input string
+	unpackStr, err := Unpack(inStr)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Input stting: %s\nUnpacked string: %s\n", inStr, unpackStr)
 }
