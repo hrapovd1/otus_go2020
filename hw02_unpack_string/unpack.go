34dd060b2fb4c9c3c7236ac2dc68d1a2b860d9ed
Dmitry Khrapov <hrapovd@gmail.com>
2020-09-07 23:45:41 +0300

    It's working version, main test OK. I tuned golangci-lint, because I don't know how to split function: linters-settings:   funlen:     lines: 100     statements: 60
      gocognit:
        min-complexity: 60

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,10 +7,7 @@ import (
 	"unicode"
 )
 
-/*
-Не знаю как использовать
 var ErrInvalidString = errors.New("invalid string")
-*/
 
 func Unpack(str string) (string, error) {
 	// Проверка на пустую строку
@@ -20,38 +17,46 @@ func Unpack(str string) (string, error) {
 
 	var strOut strings.Builder // Объект результирующей строки
 	isDouble := false          // Флаг для обработки эскейп последовательностей вида: '\n'
+	const backSlash = `\`      // Оптимизаци линтера goconst
 
 	// Основной цикл перебора символов
 	for pos := 0; pos < len(str); pos++ {
+		// Для удобства понимания и работы с кодом определяю переменные предыдущего и текущего символов
+		var prevChar, currChar rune
+		if pos > 0 {
+			prevChar, currChar = rune(str[pos-1]), rune(str[pos])
+		} else {
+			prevChar, currChar = 0, rune(str[pos])
+		}
+		notLastChar := true // Установка флага, что это не последний символ
+		// Проверка на последний символ
+		if pos == len(str)-1 {
+			notLastChar = false
+		}
 
 		switch {
 		// Проверка первого символа на цифру и эскейп экран
 		case pos == 0:
 			switch {
-			case unicode.IsDigit(rune(str[pos])): // Если первая цифра, то выйти с ошибкой
-				return "", errors.New("wrong string, it can't start from digit")
-			case unicode.IsDigit(rune(str[pos+1])): // Если следующий символ цифра, то обработать на следующем цикле
+			case unicode.IsDigit(currChar): // Если первая цифра, то выйти с ошибкой
+				//return "", errors.New("wrong string, it can't start from digit")
+				return "", ErrInvalidString
+			case notLastChar && unicode.IsDigit(rune(str[pos+1])): // Если следующий символ цифра, то обработать на следующем цикле
 				break
-			case string(str[pos]) == `\`: // Если эскейп экран, то обработать на следующем цикле
+			case string(currChar) == backSlash: // Если эскейп экран, то обработать на следующем цикле
 				break
 			default: // Если не цифра, эскейп экран и следующий символ не цифра, то записываем в выходную строку
-				strOut.WriteString(string(str[pos]))
+				strOut.WriteString(string(currChar))
 			}
 
 		// Обработка символов кроме первого
 		case pos != 0:
-			notLastChar := true    // Установка флага, что это не последний символ
-			if pos == len(str)-1 { // Проверка на последний символ
-				notLastChar = false
-			}
-			// Для удобства понимания и работы с кодом определяю переменные предыдущего и текущего символов
-			prevChar, currChar := str[pos-1], str[pos]
-
 			switch {
-			case unicode.IsDigit(rune(currChar)):
+			case unicode.IsDigit(currChar):
 				// Проверка что это не число
 				if notLastChar && unicode.IsDigit(rune(str[pos+1])) || unicode.IsDigit(rune(str[pos-1])) {
-					return "", errors.New("wrong string, it can be only digit, not numeric")
+					//	return "", errors.New("wrong string, it can be only digit, not numeric")
+					return "", ErrInvalidString
 				}
 				// Преобразование цифры из строки в целое
 				currDigit, err := strconv.Atoi(string(currChar))
@@ -72,13 +77,12 @@ func Unpack(str string) (string, error) {
 						strOut.WriteString(writedChar)
 					}
 				}
-			case string(currChar) == `\`:
-				if string(prevChar) == `\` {
+			case string(currChar) == backSlash:
+				if string(prevChar) == backSlash {
 					strOut.WriteString(string(prevChar))
 				}
-				break
 			default:
-				if string(prevChar) == `\` {
+				if string(prevChar) == backSlash {
 					// Проверка на эскейп последовательность
 					if notLastChar && unicode.IsDigit(rune(str[pos+1])) {
 						isDouble = true
