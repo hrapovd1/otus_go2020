75df31f9aeb2f64594aa56a580c3b8789fc76e9f
Dmitry Khrapov <hrapovd@gmail.com>
2020-09-17 23:38:36 +0300

    Final work edition base task

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,48 +6,50 @@ import (
 )
 
 func Top10(inStr string) []string {
+	// Прооверка на пустую строку
 	if len(inStr) == 0 {
 		return []string{}
 	}
+
+	// Структура для слова и его счетчика
 	type wordRate struct {
 		Word  string
 		Count int
 	}
-	words := make([]wordRate, 1)
-	var outStr, tmpWords []string
-	tmpWordsMap := make(map[string]int)
-	tmpLines := strings.Split(strings.ToLower(inStr), "\n")
 
+	words := make([]wordRate, 1)                   // Слайс словесных структур
+	var outStr, tmpWords []string                  // Переменные используемые в обработке
+	tmpWordsMap := make(map[string]int)            // Словарь слов и их количества
+	tmpLines := strings.Split(inStr, string('\n')) // Разбиваю текст на строки
+
+	// Разделяю строки на слова во временный слайс
 	for _, sentence := range tmpLines {
-		for _, word := range strings.Split(sentence, " ") {
+		for _, word := range strings.Split(sentence, string(' ')) {
 			if len(word) > 0 {
 				tmpWords = append(tmpWords, strings.TrimSpace(word))
 			}
 		}
 	}
 
+	// Собираю слова в словарь с одновременным подсчетом одинаковых
 	for i := 0; i < len(tmpWords); i++ {
-		/*
-			for _, word := range strings.Split(tmpWords[i], ".") {
-				if len(word) > 0 {
-					tmpWordsMap[word]++
-				}
-			}
-		*/
 		if len(tmpWords[i]) > 0 {
 			tmpWordsMap[tmpWords[i]]++
 		}
 	}
 
-	//fmt.Println("{------- start -----------}")
+	// Преобразую словарь слов в слайс структуры слово-счетчик для сортировки
 	for word, count := range tmpWordsMap {
 		words = append(words, wordRate{Word: word, Count: count})
 	}
+	// Сортировка слайса структур по счетчику слов от большего к меньшему
 	sort.Slice(words, func(i, j int) bool {
 		return words[i].Count > words[j].Count
 	})
+
+	// Беру первые 10 слов из сортированного по счетчику слайса
+	// и возврощаю полученный результат
 	for i := 0; i < 10; i++ {
-		//	fmt.Printf("Word: %s  ; Count: %d\n", words[i].Word, words[i].Count)
 		outStr = append(outStr, words[i].Word)
 	}
 	return outStr
