f8d4e6f153642db1bc14f768f0c54150c1d2e0f0
Dmitry Khrapov <hrapovd@gmail.com>
2020-10-01 21:56:23 +0300

    Describe functions.

--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/hrapovd1/otus_go2020/hw04_lru_cache
 
 go 1.14
 
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,17 +1,57 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int                          // длина списка
+	Front() *listItem                  // первый элемент списка
+	Back() *listItem                   // последний элемент списка
+	PushFront(v interface{}) *listItem // добавить значение в начало
+	PushBack(v interface{}) *listItem  // добавить значение в конец
+	Remove(i *listItem)                // удалить элемент
+	MoveToFront(i *listItem)           // переместить элемент в начало
 }
 
 type listItem struct {
-	// Place your code here
+	element int
 }
 
 type list struct {
-	// Place your code here
+	current *listItem
+	next    *listItem
+	prev    *listItem
+	count   *int
 }
 
 func NewList() List {
-	return &list{}
+	count := 0
+	return &list{next: nil, prev: nil, current: &listItem{}, count: &count}
+}
+
+func (l list) Len() int {
+	return *l.count
+}
+
+func (l list) Front() *listItem {
+	return &listItem{}
+}
+
+func (l list) Back() *listItem {
+	return &listItem{}
+}
+
+func (l list) PushFront(v interface{}) *listItem {
+	newItem := listItem{element: v.(int)}
+	*l.count++
+	l.next = l.prev
+	l.prev = nil
+	return &newItem
+}
+
+func (l list) PushBack(v interface{}) *listItem {
+	return &listItem{}
+}
+
+func (l list) Remove(i *listItem) {
+}
+
+func (l list) MoveToFront(i *listItem) {
 }
