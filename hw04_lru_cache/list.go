163ffa3e0056c3f2c3cadf11abcc9ccedc9f5e6c
Dmitry Khrapov <hrapovd@gmail.com>
2020-10-05 22:40:41 +0300

    Wrong code! It relies to slice order.

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -12,46 +12,69 @@ type List interface {
 
 type listItem struct {
 	element int
+	next    *listItem
+	prev    *listItem
 }
 
+type list struct {
+	list []listItem
+}
+
+/*
 type list struct {
 	current *listItem
 	next    *listItem
 	prev    *listItem
 	count   *int
 }
+*/
 
 func NewList() List {
-	count := 0
-	return &list{next: nil, prev: nil, current: &listItem{}, count: &count}
+	return list{make([]listItem, 0)}
 }
 
 func (l list) Len() int {
-	return *l.count
+	return len(l.list)
 }
 
 func (l list) Front() *listItem {
-	return &listItem{}
+	return &l.list[0]
 }
 
 func (l list) Back() *listItem {
-	return &listItem{}
+	return &l.list[l.Len()-1]
 }
 
 func (l list) PushFront(v interface{}) *listItem {
-	newItem := listItem{element: v.(int)}
-	*l.count++
-	l.next = l.prev
-	l.prev = nil
-	return &newItem
+	item := listItem{element: v.(int), prev: nil, next: &l.list[0]}
+	l.list[0].prev = &item
+	tmp := make([]listItem, l.Len()+1)
+	tmp = append(tmp, item)
+	l.list = append(tmp, l.list...)
+	return &item
 }
 
 func (l list) PushBack(v interface{}) *listItem {
-	return &listItem{}
+	item := listItem{element: v.(int), next: nil, prev: &l.list[l.Len()-1]}
+	l.list[l.Len()-1].next = &item
+	tmp := make([]listItem, l.Len()+1)
+	tmp = append(tmp, l.list...)
+	l.list = append(tmp, item)
+	return &item
 }
 
 func (l list) Remove(i *listItem) {
+	//TODO: need Remove code
+	if l.Len() != 0 {
+		tmp := make([]listItem, l.Len()-1)
+
+	}
 }
 
 func (l list) MoveToFront(i *listItem) {
+	//TODO: need MoveToFront code
+	switch l.Len() {
+	case 1:
+	case 2:
+	}
 }
